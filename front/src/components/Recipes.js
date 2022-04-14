068a2d324e7426945aaa37cebc403b805d403611
KamargakisTheo <theokamargakis@gmail.com>
2022-04-14 13:48:14 +0200

    display data from api

---
+++ front/src/components/Recipes.js
@@ -0,0 +1,48 @@
+import React from "react";
+class App extends React.Component {
+   
+
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+
+    componentDidMount() {
+        fetch(
+"http://localhost:1337/api/recipes")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Chargement... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> Liste des recettes : </h1>  {
+                items.data.map((item) => ( 
+                <ol key = { item.id } >
+                    { item.attributes.recipe_name }<br></br>
+                    { item.attributes.nationality }<br></br>
+                    { item.attributes.quantity }<br></br>
+                    { item.attributes.description }<br></br>
+                </ol>
+                ))
+            }
+        </div>
+    );
+}
+}
+   
+export default App;
