e4951a8d1705c736aa6c584438aa71fabbf2973f
KamargakisTheo <theokamargakis@gmail.com>
2022-04-15 15:56:28 +0200

    display data

--- front/src/components/Recipes.js
+++ front/src/components/Recipes.js
@@ -1,20 +1,19 @@
 import React from "react";
+
 class App extends React.Component {
-   
 
     constructor(props) {
         super(props);
-   
+
         this.state = {
             items: [],
             DataisLoaded: false
         };
     }
-   
 
     componentDidMount() {
         fetch(
-"http://localhost:1337/api/recipes")
+                "http://localhost:1337/api/recipes")
             .then((res) => res.json())
             .then((json) => {
                 this.setState({
@@ -25,24 +24,26 @@ class App extends React.Component {
     }
     render() {
         const { DataisLoaded, items } = this.state;
-        if (!DataisLoaded) return <div>
-            <h1> Chargement... </h1> </div> ;
-   
-        return (
-        <div className = "App">
-            <h1> Liste des recettes : </h1>  {
-                items.data.map((item) => ( 
-                <ol key = { item.id } >
-                    { item.attributes.recipe_name }<br></br>
-                    { item.attributes.nationality }<br></br>
-                    { item.attributes.quantity }<br></br>
-                    { item.attributes.description }<br></br>
-                </ol>
+        if (!DataisLoaded) return <div >
+            <h1> Chargement... </h1> </div > ;
+
+        return ( <div className = "App" >
+                <h1 > Liste des recettes: </h1>  {
+                items.data.map((item) => (
+
+                    <div key = { item.id } >
+                    { item.attributes.recipe_name } < br > </br> 
+                    { item.attributes.nationality } < br > </br > 
+                    { item.attributes.quantity } < br > </br> 
+                    { item.attributes.description } < br > </br > 
+                    { item.attributes.author } < br > </br>
+                    </div>
+
                 ))
-            }
-        </div>
+
+            }</div>
     );
 }
 }
-   
+
 export default App;
